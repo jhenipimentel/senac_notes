3fde3f6ea101a529a29c830a93db6c17c1c0c36a
JHENIFER BIANCA DE ASSIS PIMENTEL <jhenifer.2986@aluno.pr.senac.br>
2022-07-26 22:21:30 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Login')
+})
+app.get('/notes', (req, res) => {
+    res.send('Notes')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
